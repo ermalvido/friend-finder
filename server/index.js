036ccd40187d14f1136738fef2aeab0490be01e1
ermalvido <ermalvido@gmail.com>
2020-11-03 14:11:18 -0700

    hosting

--- server/index.js
+++ server/index.js
@@ -5,11 +5,14 @@ const express = require('express'),
       authCtrl = require('./controllers/authCtrl'),
       postCtrl = require('./controllers/postCtrl'),
       nmCtrl = require('./controllers/nmCtrl'),
+      path = require('path'),
     { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env,
       port = SERVER_PORT,
       app = express();
 
 app.use(express.json());
+
+app.use(express.static(__dirname + '/../build'));
 
 app.use(session({
     resave: false,
@@ -40,5 +43,9 @@ app.delete('/api/post/:id', postCtrl.deletePost);
 app.put('/api/user/:id', postCtrl.updateUser);
 
 app.post('/api/email', nmCtrl.email);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 app.listen(port, () => console.log(`Server running on ${port}`));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './ducks/store';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const Router = 
+  process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
 
 const theme = createMuiTheme({
   palette: {
